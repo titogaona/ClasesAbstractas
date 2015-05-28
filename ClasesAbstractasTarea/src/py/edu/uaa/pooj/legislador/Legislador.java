cd71f7848bcccd32e54479a8a916d09d67cb7e0b
titogaona <tito-gaona@hotmail.es>
2015-05-27 23:40:36 -0400

    Clases Abstractas
    Herencia

---
+++ ClasesAbstractasTarea/src/py/edu/uaa/pooj/legislador/Legislador.java
@@ -0,0 +1,16 @@
+package py.edu.uaa.pooj.legislador;
+
+public class Legislador extends Persona {
+
+public int codigo;
+	
+	
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+}
